6ade0d645fbb67bf46095875d294973e2f9cc4f9
Guilherme Sona <Guisona742121@gmail.com>
2026-04-07 16:39:10 -0300

    Feat: Implementa weather card e logica de data fetching

---
+++ components/WeatherCard.tsx
@@ -0,0 +1,12 @@
+export default function WeatherCard({ data }: any) {
+  return (
+    <div className="mt-6 p-6 bg-white rounded-xl shadow-md flex flex-col items-center">
+      <h2 className="text-2xl font-bold text-gray-800">{data.name}</h2>
+      <p className="text-5xl font-extrabold text-blue-600 my-4">
+        {Math.round(data.main.temp)}°C
+      </p>
+      <p className="text-gray-500 capitalize">{data.weather[0].description}</p>
+      {/* Ícone da API: https://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png */}
+    </div>
+  );
+}
